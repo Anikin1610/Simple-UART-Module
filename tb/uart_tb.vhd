54a9a5c5edf297ef6b67343327bc1f7a3239bb74
Anirudh <ianirudh16@gmail.com>
2020-12-08 09:18:44 +0530

    A simple test bench to simulate the functionality of the UART Module

--- tb/uart_tb.vhd
+++ tb/uart_tb.vhd
@@ -3,35 +3,37 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use STD.ENV.FINISH;
 
---use IEEE.NUMERIC_STD.ALL;
-
 entity uart_tb is
 end uart_tb;
 
 architecture tb_beh of uart_tb is
     constant baud_rate_tb : integer := 9600;
     constant clk_rate_tb : integer := 12e6;
-    constant baud : time := 104.16666us;
+    constant baud : time := 104.16666us;			-- (1 / 9600) seconds
     signal clk_12MHz : STD_LOGIC := '0';
     signal rst, start_tx, rx, tx, rx_busy, tx_busy, rx_invalid : STD_LOGIC := '0';
     signal rx_data, tx_data : STD_LOGIC_VECTOR(7 downto 0);
 begin
-	
-	rx <= tx;
-	
+    
+    rx <= tx;			-- Gives the serial output of the transmitter back to the serial input of reciever 
+    
     DUT:entity work.UART 
             generic map	(  baud_rate => baud_Rate_tb,
-					       clk_rate => clk_Rate_tb)
+                           clk_rate => clk_Rate_tb)
             Port map ( 	clk => clk_12MHz,
-			            rst => rst,
+                        rst => rst,
                         rx => rx,
                         start_tx => start_tx,
-			            rx_busy => rx_busy,
-			            rx_invalid => rx_invalid,
+                        rx_busy => rx_busy,
+                        rx_invalid => rx_invalid,
                         rx_data => rx_data,
                         tx_busy => tx_busy,
                         tx_data => tx_data,
-			            tx => tx);
+                        tx => tx);
+                        
+    ---------------------------------------------------------------------------------------------------------------------------------------------------------
+    -- Process to generate 12MHz clock signal.
+    ---------------------------------------------------------------------------------------------------------------------------------------------------------	
     clk_gen:process
     begin
         clk_12MHz <= '0';
@@ -79,26 +81,26 @@ begin
                 
 --    end process tb_proc;
 
-	tb_proc:process
-	begin
-		rst <= '1';
+    tb_proc:process
+    begin
+        rst <= '1';
         wait for 10 us;
-	    rst <= '0';
-	    wait for 3 * baud;
-		tx_data <= "01010101";
-		wait for 2 * baud;
-		start_tx <= '1';
-		wait for baud;
-		start_tx <= '0';
-		wait for 1 ms;
-		tx_data <= "01101001";
-		wait for 2 * baud;
-		start_tx <= '1';
-		wait for baud;
-		start_tx <= '0';
-		wait for 5 ms;
-		
-		finish;
-	end process tb_proc;
-	
+        rst <= '0';
+        wait for 3 * baud;
+        tx_data <= "01010101";
+        wait for 2 * baud;
+        start_tx <= '1';
+        wait for baud;
+        start_tx <= '0';
+        wait for 1 ms;
+        tx_data <= "01101001";
+        wait for 2 * baud;
+        start_tx <= '1';
+        wait for baud;
+        start_tx <= '0';
+        wait for 5 ms;
+        
+        finish;
+    end process tb_proc;
+    
 end tb_beh;
